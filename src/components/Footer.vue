39584f565f7bbb80bca4063e201027fddbe134ce
Alex Okeyo <alexokeyo15@gmail.com>
2024-05-22 15:48:06 +0300

    footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="overflow-hidden">
-    <!-- component -->
+  <!----<div class="overflow-hidden">
+    <!-- component --
     <footer
       class="bg-gradient-to-r from-teal-500 via-teal-600 to-teal-700 overflow-hidden"
     >
@@ -14,7 +14,7 @@
             <img src="@/assets/ElimishaWatotoLogoWhite.png" alt="logo" class=""/>
             <!----<p class="max-w-xs mt-4 text-sm text-white">
               Educate a child, transform a nation.
-            </p>-->
+            </p>--
             <div class="flex mt-8 space-x-6 text-white">
               <a class="hover:opacity-75" href="https://www.linkedin.com/in/elimisha-watoto-foundation-b26b972a0/edit/forms/contact-info/new/?profileFormEntryPoint=PROFILE_TOP_CARD_EDIT_FORM" target="_blank" rel="noreferrer">
                 <svg 
@@ -73,22 +73,22 @@
             <div>
               <p class="text-xl font-medium">Quick Links</p>
               <nav class="flex flex-col mt-4 space-y-2 text-sm text-white">
-                <a @click="goToHome()" class="hover:opacity-75" href="">
+                <a @click="goToHome()" class="hover:opacity-75" href="/">
                   Home
                 </a>
-                <a @click="goToAbout()" class="hover:opacity-75" href>
+                <a @click="goToAbout()" class="hover:opacity-75" href="/about">
                   Who We Are
                 </a>
-                <a @click="goToWork()" class="hover:opacity-75" href>
+                <a @click="goToWork()" class="hover:opacity-75" href="/work">
                   What We Do
                 </a>
-                <a @click="goToInvolved()" class="hover:opacity-75" href>
+                <a @click="goToInvolved()" class="hover:opacity-75" href="/involved">
                   Get Involved
                 </a>
-                <a @click="goToProgrammes()" class="hover:opacity-75" href>
+                <a @click="goToProgrammes()" class="hover:opacity-75" href="/programmes">
                   Our Programmes
                 </a>
-                <a @click="goToFAQs()" class="hover:opacity-75" href> FAQs </a>
+                <a @click="goToFAQs()" class="hover:opacity-75" href="faqs"> FAQs </a>
               </nav>
             </div>
             <div>
@@ -123,8 +123,139 @@
         <p class="mt-8 text-xl font-medium text-gray-800">© 2024 | Elimisha Watoto Foundation.</p>
       </div>
     </footer>
-  </div>
+  </div>-->
+    <footer class="bg-gradient-to-r from-teal-500 via-teal-600 to-teal-700 overflow-hidden">
+    <div class="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+      <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div>
+          <div class="text-teal-600">
+            <img src="@/assets/ElimishaWatotoLogoWhite.png" alt="logo" class="w-64"/>
+          </div>
+          <ul class="mt-8 flex gap-6">
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100095380491032"
+                rel="noreferrer"
+                target="_blank"
+                class="text-white transition hover:opacity-75"
+              >
+                <span class="sr-only">Facebook</span>
+  
+                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path
+                    fill-rule="evenodd"
+                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+  
+            <li>
+              <a
+                href="https://www.instagram.com/friendsofelimisha/"
+                rel="noreferrer"
+                target="_blank"
+                class="text-white transition hover:opacity-75"
+              >
+                <span class="sr-only">Instagram</span>
+  
+                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+  
+
+          </ul>
+        </div>
+  
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-3">
+          <div>
+            <p class="font-medium text-xl text-black">Quick Links</p>
+  
+            <ul class="mt-6 space-y-4 text-sm">
+              <nav class="flex flex-col mt-4 space-y-2 text-sm text-white">
+                <a @click="goToHome()" class="hover:opacity-75" href="/">
+                  Home
+                </a>
+                <a @click="goToAbout()" class="hover:opacity-75" href="/about">
+                  Who We Are
+                </a>
+                <a @click="goToWork()" class="hover:opacity-75" href="/work">
+                  What We Do
+                </a>
+                <a @click="goToInvolved()" class="hover:opacity-75" href="/involved">
+                  Get Involved
+                </a>
+                <a @click="goToProgrammes()" class="hover:opacity-75" href="/programmes">
+                  Our Programmes
+                </a>
+                <a @click="goToFAQs()" class="hover:opacity-75" href="faqs"> FAQs </a>
+              </nav>
+            </ul>
+          </div>
+  
+          <div>
+            <p class="font-medium text-xl text-black">Contact Us</p>
+  
+            <ul class="mt-6 space-y-4">
+              <nav class="flex flex-col mt-4 space-y-2 text-sm text-white">
+                <p>
+                  Email:
+                  <a class="hover:opacity-75" href="mailto:info@elimishawatoto.org"> info@elimishawatoto.org </a>
+                </p>
+                <p>Phone: <a class="hover:opacity-75" href="tel:+254 753 343 152"> +254 753 343 152 </a></p>
+                <p>
+                  Location: 3rd Floor, Woodvale Place Woodvale Grove, Westlands,
+                  Nairobi
+                </p>
+              </nav>
+            </ul>
+          </div>
+  
+          <div>
+            <p class="font-medium text-xl text-black">Our Location</p>
+  
+            <ul class="mt-6 space-y-4 text-sm">
+              <nav class="flex flex-col mt-4 space-y-2 text-sm">
+                <div class="md:block hidden">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.683673920301!2d36.80307799116752!3d-1.267658744881094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17155bc87d13%3A0xc8aec5a75ccd330e!2sWoodvale%20Place%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1709373114541!5m2!1sen!2ske"
+                    width="300px"
+                    height="300"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+                <div class="block md:hidden">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.683673920301!2d36.80307799116752!3d-1.267658744881094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17155bc87d13%3A0xc8aec5a75ccd330e!2sWoodvale%20Place%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1709373114541!5m2!1sen!2ske"
+                    width="300"
+                    height="300"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              </nav>
+            </ul>
+          </div>
+        </div>
+      </div>
+  
+      <p class="text-base  text-black">&copy; 2024. Elimisha Watoto Foundation. All rights reserved.</p>
+    </div>
+  </footer>
 </template>
+
 
 <script>
 import router from "../plugins/router.js";
@@ -159,5 +290,9 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
-./Footer.vue/index.js
+<style scoped>
+
+
+
+</style>
+
