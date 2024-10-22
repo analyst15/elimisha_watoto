ddbcf40cf53efceb9d50f2d2a6ed3f72272c8751
Alex Okeyo <alexokeyo15@gmail.com>
2024-10-22 16:23:23 +0300

    added country to form fields

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -139,17 +139,17 @@
               </div>
               <div class="sm:col-span-2">
                 <label
-                  for="email"
+                  for="country"
                   class="block text-sm font-semibold leading-6 text-gray-900"
                   >Country</label
                 >
                 <div class="mt-2.5">
                   <input
-                    type="email"
-                    id="email"
+                    type="text"
+                    id="country"
                     autocomplete="email"
                     class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    name="email"
+                    name="country"
                     v-model="uEmail" />
                 </div>
               </div>
