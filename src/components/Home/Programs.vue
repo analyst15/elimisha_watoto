9e2f795c7ea8a46ca8467c935b9ae2dbe062a3cc
Alex Okeyo <alexokeyo15@gmail.com>
2024-11-25 12:21:20 +0300

    Updated the programs

--- src/components/Home/Programs.vue
+++ src/components/Home/Programs.vue
@@ -7,17 +7,17 @@
         <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
             <i class="material-icons text-emerald-700 text-8xl">laptop_mac</i>
             <h1 class="mb-4 font-bold">Student Support</h1>
-            <p class="mx-8">At the heart of Elimisha Watoto Foundation is the office of student liaison. Our team dedicates their time and energy in capacity building for each and every one of our students.</p>
+            <p class="mx-8">At the heart of Elimisha Watoto Foundation is the Office of Student Liaison. The Student Liaison Officer is the link between the Foundation, our beneficiaries and their parents, and the school administration</p>
         </div>
         <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
             <i class="material-icons text-emerald-700 text-8xl">local_library</i>
             <h1 class="mb-4 font-bold">Academic Mentorship</h1>
-            <p class="mx-8">Elimisha Watoto Foundation is focused on supporting needy but academic brilliant students. Our cut off merit mark is 300 and above  marks in the Kenya Certificate of Primary School Education. We believe in growing the academic potential of our students.</p>
+            <p class="mx-8">We are dedicated to enhancing academic performance of our beneficiaries by providing personalized mentorship and actively monitoring their welfare.</p>
         </div>
         <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
             <i class="material-icons text-emerald-700 text-8xl">auto_stories</i>
             <h1 class="mb-4 font-bold">Leap</h1>
-            <p class="mx-8">The Life Experience And Preparation Programme is a training programme set up to equip individuals with necessary soft skills preparing them to fit-in anywhere as global citizens.</p>
+            <p class="mx-8">The Life Experience and Preparation Programme is a key component of our capacity-building efforts, aimed at empowering our beneficiaries with skills and knowledge to navigate their life journey successfully.</p>
         </div>
     </div>
 </template>
