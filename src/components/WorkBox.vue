77a90d6403f0f8e66f4d855a737f15908ace5772
Alex Okeyo <alexokeyo15@gmail.com>
2024-12-16 16:51:37 +0300

    Added an additional box

--- src/components/WorkBox.vue
+++ src/components/WorkBox.vue
@@ -11,7 +11,7 @@
           class="flex break-inside bg-teal-700 hover:bg-teal-400 border-gray-50 border-2 rounded-3xl px-4 py-6 mb-4 ml-3 mt-10">
           <div class="flex items-center justify-between flex-1">
             <span class="text-lg font-medium text-white mr-2">
-              Student Identification</span
+              Identifying the Students</span
             >
             <svg
               width="17"
@@ -141,7 +141,7 @@
           class="flex break-inside bg-teal-700 hover:bg-teal-400 border-gray-50 border-2 rounded-3xl px-4 py-6 mb-4 ml-3 mt-10">
           <div class="flex items-center justify-between flex-1">
             <span class="text-lg font-medium text-white mr-2">
-              Meet the Parent
+              Meeting the Parents
             </span>
             <svg
               width="17"
@@ -309,94 +309,6 @@
           </div>
         </div>
 
-        <!-- Metting the Headteacher -->
-        <button
-          @click="pop4 = !pop4"
-          type="button"
-          class="flex break-inside bg-teal-700 hover:bg-teal-400 border-gray-50 border-2 rounded-3xl px-4 py-6 mb-4 ml-3 mt-10">
-          <div class="flex items-center justify-between flex-1">
-            <span class="text-lg font-medium text-white mr-2">
-              Meeting the Headteacher
-            </span>
-            <svg
-              width="17"
-              height="17"
-              viewBox="0 0 17 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M0 8.71423C0 8.47852 0.094421 8.25246 0.262491 8.08578C0.430562 7.91911 0.658514 7.82547 0.896201 7.82547H13.9388L8.29808 2.23337C8.12979 2.06648 8.03525 1.84013 8.03525 1.60412C8.03525 1.36811 8.12979 1.14176 8.29808 0.974875C8.46636 0.807989 8.6946 0.714233 8.93259 0.714233C9.17057 0.714233 9.39882 0.807989 9.5671 0.974875L16.7367 8.08499C16.8202 8.16755 16.8864 8.26562 16.9316 8.3736C16.9767 8.48158 17 8.59733 17 8.71423C17 8.83114 16.9767 8.94689 16.9316 9.05487C16.8864 9.16284 16.8202 9.26092 16.7367 9.34348L9.5671 16.4536C9.39882 16.6205 9.17057 16.7142 8.93259 16.7142C8.6946 16.7142 8.46636 16.6205 8.29808 16.4536C8.12979 16.2867 8.03525 16.0604 8.03525 15.8243C8.03525 15.5883 8.12979 15.362 8.29808 15.1951L13.9388 9.603H0.896201C0.658514 9.603 0.430562 9.50936 0.262491 9.34268C0.094421 9.17601 0 8.94995 0 8.71423Z"
-                fill="white" />
-            </svg>
-          </div>
-        </button>
-        <div
-          v-if="pop4"
-          @click="closePopup"
-          class="absolute bg-black bg-opacity-50 flex items-center justify-center">
-          <div
-            class="relative flex max-w-[48rem] flex-row bg-orange-600 bg-clip-border text-white font-serif shadow-md">
-            <div class="p-6 mt-4 mb-4">
-              <ul class="list-item">
-                <li class="custom-bullet">
-                  <svg
-                    class="inline-block mr-2 h-3 w-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512">
-                    <path
-                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
-                  </svg>
-                  With consent from the parent and after meeting the student,
-                  the liaison officer contacts the school administrator to for a
-                  formal visit to the school.
-                </li>
-
-                <li class="custom-bullet mt-4">
-                  <svg
-                    class="inline-block mr-2 h-3 w-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512">
-                    <path
-                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
-                  </svg>
-                  This visitation is aimed at validating parent consent of the
-                  Foundation’s involvement in academic and financial matters in
-                  concern with the student
-                </li>
-
-                <li class="custom-bullet mt-4">
-                  <svg
-                    class="inline-block mr-2 h-3 w-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512">
-                    <path
-                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
-                  </svg>
-                  The headteacher’s role involves assigning a dedicated teacher
-                  to the Foundation with to whom we will communicate and make
-                  requests . The teacher is required to share termly fees
-                  statements, termly report cards and receipts as well any extra
-                  support the student may require.
-                </li>
-
-                <li class="custom-bullet mt-4">
-                  <svg
-                    class="inline-block mr-2 h-3 w-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512">
-                    <path
-                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
-                  </svg>
-                  In the spirit of loco parentis, the teacher will be required
-                  to give a detailed termly report on the student's academic
-                  progress.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
 
         <!-- Student Profle -->
         <button
@@ -405,7 +317,7 @@
           class="flex break-inside bg-teal-700 hover:bg-teal-400 border-gray-50 border-2 rounded-3xl px-4 py-6 mb-4 ml-3 mt-10">
           <div class="flex items-center justify-between flex-1">
             <span class="text-lg font-medium text-white mr-2">
-              The Student Profile
+              Setting Student Profile
             </span>
             <svg
               width="17"
@@ -534,6 +446,187 @@
             </div>
           </div>
         </div>
+
+        <!-- Metting the Headteacher -->
+        <button
+          @click="pop4 = !pop4"
+          type="button"
+          class="flex break-inside bg-teal-700 hover:bg-teal-400 border-gray-50 border-2 rounded-3xl px-4 py-6 mb-4 ml-3 mt-10">
+          <div class="flex items-center justify-between flex-1">
+            <span class="text-lg font-medium text-white mr-2">
+              Meeting the Headteacher
+            </span>
+            <svg
+              width="17"
+              height="17"
+              viewBox="0 0 17 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0 8.71423C0 8.47852 0.094421 8.25246 0.262491 8.08578C0.430562 7.91911 0.658514 7.82547 0.896201 7.82547H13.9388L8.29808 2.23337C8.12979 2.06648 8.03525 1.84013 8.03525 1.60412C8.03525 1.36811 8.12979 1.14176 8.29808 0.974875C8.46636 0.807989 8.6946 0.714233 8.93259 0.714233C9.17057 0.714233 9.39882 0.807989 9.5671 0.974875L16.7367 8.08499C16.8202 8.16755 16.8864 8.26562 16.9316 8.3736C16.9767 8.48158 17 8.59733 17 8.71423C17 8.83114 16.9767 8.94689 16.9316 9.05487C16.8864 9.16284 16.8202 9.26092 16.7367 9.34348L9.5671 16.4536C9.39882 16.6205 9.17057 16.7142 8.93259 16.7142C8.6946 16.7142 8.46636 16.6205 8.29808 16.4536C8.12979 16.2867 8.03525 16.0604 8.03525 15.8243C8.03525 15.5883 8.12979 15.362 8.29808 15.1951L13.9388 9.603H0.896201C0.658514 9.603 0.430562 9.50936 0.262491 9.34268C0.094421 9.17601 0 8.94995 0 8.71423Z"
+                fill="white" />
+            </svg>
+          </div>
+        </button>
+        <div
+          v-if="pop4"
+          @click="closePopup"
+          class="absolute bg-black bg-opacity-50 flex items-center justify-center">
+          <div
+            class="relative flex max-w-[48rem] flex-row bg-orange-600 bg-clip-border text-white font-serif shadow-md">
+            <div class="p-6 mt-4 mb-4">
+              <ul class="list-item">
+                <li class="custom-bullet">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  With consent from the parent and after meeting the student,
+                  the liaison officer contacts the school administrator to for a
+                  formal visit to the school.
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  This visitation is aimed at validating parent consent of the
+                  Foundation’s involvement in academic and financial matters in
+                  concern with the student
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  The headteacher’s role involves assigning a dedicated teacher
+                  to the Foundation with to whom we will communicate and make
+                  requests . The teacher is required to share termly fees
+                  statements, termly report cards and receipts as well any extra
+                  support the student may require.
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  In the spirit of loco parentis, the teacher will be required
+                  to give a detailed termly report on the student's academic
+                  progress.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+          <!-- Folloowing up on Students -->
+          <button
+          @click="pop4 = !pop4"
+          type="button"
+          class="flex break-inside bg-teal-700 hover:bg-teal-400 border-gray-50 border-2 rounded-3xl px-4 py-6 mb-4 ml-3 mt-10">
+          <div class="flex items-center justify-between flex-1">
+            <span class="text-lg font-medium text-white mr-2">
+              Following up on Students
+            </span>
+            <svg
+              width="17"
+              height="17"
+              viewBox="0 0 17 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0 8.71423C0 8.47852 0.094421 8.25246 0.262491 8.08578C0.430562 7.91911 0.658514 7.82547 0.896201 7.82547H13.9388L8.29808 2.23337C8.12979 2.06648 8.03525 1.84013 8.03525 1.60412C8.03525 1.36811 8.12979 1.14176 8.29808 0.974875C8.46636 0.807989 8.6946 0.714233 8.93259 0.714233C9.17057 0.714233 9.39882 0.807989 9.5671 0.974875L16.7367 8.08499C16.8202 8.16755 16.8864 8.26562 16.9316 8.3736C16.9767 8.48158 17 8.59733 17 8.71423C17 8.83114 16.9767 8.94689 16.9316 9.05487C16.8864 9.16284 16.8202 9.26092 16.7367 9.34348L9.5671 16.4536C9.39882 16.6205 9.17057 16.7142 8.93259 16.7142C8.6946 16.7142 8.46636 16.6205 8.29808 16.4536C8.12979 16.2867 8.03525 16.0604 8.03525 15.8243C8.03525 15.5883 8.12979 15.362 8.29808 15.1951L13.9388 9.603H0.896201C0.658514 9.603 0.430562 9.50936 0.262491 9.34268C0.094421 9.17601 0 8.94995 0 8.71423Z"
+                fill="white" />
+            </svg>
+          </div>
+        </button>
+        <div
+          v-if="pop4"
+          @click="closePopup"
+          class="absolute bg-black bg-opacity-50 flex items-center justify-center">
+          <div
+            class="relative flex max-w-[48rem] flex-row bg-orange-600 bg-clip-border text-white font-serif shadow-md">
+            <div class="p-6 mt-4 mb-4">
+              <ul class="list-item">
+                <li class="custom-bullet">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  Effective follow-ups are crucial for engaging students, parents, and teachers through direct communication like phone calls and meetings.
+                  This process helps assess and monitor students’ academic and emotional progress, allowing for a better understanding of their well-being. 
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  The student liaison office also organizes regular school visits to strengthen relationships within the educational community which includes teachers. 
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  These visits provide opportunities for our officers to connect with students at a personal level. 
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  By prioritizing follow-ups and visits, we not only foster personal development but also empower students to make informed educational choices, enhancing their emotional and social well-being
+                </li>
+
+                <li class="custom-bullet mt-4">
+                  <svg
+                    class="inline-block mr-2 h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                  The liaison office plays a key role in ensuring students feel connected and supported throughout their academic journey.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
