44d97b39dfdc15cae449ce49b84541e124a18f3c
Alex Okeyo <alexokeyo15@gmail.com>
2024-12-16 16:58:22 +0300

    Created two files in the vieews folder Mentorship and PsychosocialSupport

---
+++ src/views/Mentorship.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="px-4 md:px-8 lg:px-16 xl:px-32 pt-40">
+      <div class="text-center font-bold font-sans text-4xl text-teal-600">
+        Mentorship
+      </div>
+      <div class="p-10">
+        <p class="m-5">
+            Our mentorship program is designed to support our students in navigating their career paths and fostering their 
+            leadership skills. This comprehensive initiative provides our students with personalized guidance, insight into various industries, and strategies for professional development. It ensures that the scholarship experience is not only focused on education but also emphasizes practical skills and personal growth, ultimately preparing students for successful futures in their chosen fields.
+        </p>
+      </div>
+      <div class="flex grid-col-1 gap-10 px-10 justify-center flex-wrap mb-20">
+        <img
+          src="https://static.wixstatic.com/media/51210f_d8843407b0d34784b9e4d09174f12e82~mv2.jpg/v1/fill/w_342,h_218,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/51210f_d8843407b0d34784b9e4d09174f12e82~mv2.jpg" />
+        <img
+          src="https://static.wixstatic.com/media/51210f_1f8b44e326164cabab275cd35dd397e9~mv2.jpg/v1/fill/w_327,h_218,al_c,lg_1,q_80,enc_auto/51210f_1f8b44e326164cabab275cd35dd397e9~mv2.jpg" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      return {};
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
+  <style lang="scss" scoped></style>
+  
---
+++ src/views/PsychosocialSupport.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="px-4 md:px-8 lg:px-16 xl:px-32 pt-40">
+      <div class="text-center font-bold font-sans text-4xl text-teal-600">
+        Psychosocial Support
+      </div>
+      <div class="p-10">
+        <p class="m-5">
+            Recognizing that many of our students come from challenging backgrounds, we understand that circumstances at home can greatly affect their mental well-being and academic performance.
+            When we observe a drop in a student's performance, our dedicated liaison officers reach out to assess their situation, whether through phone calls or in-person meetings. This compassionate 
+            approach helps us understand their needs better and determine the most appropriate steps to take, which may involve connecting them with professional counsellors, as we believe that a healthy 
+            state of mind is essential for academic success. Our collaboration with Amani Counselling Centre, has been invaluable in providing the much-needed psychosocial support for our students, helping
+            them navigate their challenges and thrive. 
+        </p>
+      </div>
+      <!-- <div class="flex grid-col-1 gap-10 px-10 justify-center flex-wrap mb-20">
+        <img
+          src="https://static.wixstatic.com/media/51210f_d8843407b0d34784b9e4d09174f12e82~mv2.jpg/v1/fill/w_342,h_218,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/51210f_d8843407b0d34784b9e4d09174f12e82~mv2.jpg" />
+        <img
+          src="https://static.wixstatic.com/media/51210f_1f8b44e326164cabab275cd35dd397e9~mv2.jpg/v1/fill/w_327,h_218,al_c,lg_1,q_80,enc_auto/51210f_1f8b44e326164cabab275cd35dd397e9~mv2.jpg" />
+      </div>-->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      return {};
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
+  <style lang="scss" scoped></style>
+  
