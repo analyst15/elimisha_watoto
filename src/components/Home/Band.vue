8de9267da5d7d4e8f1db68a6e3a0705f17412111
Alex Okeyo <alexokeyo15@gmail.com>
2025-02-11 16:39:35 +0300

    capitalize on child and nation

--- src/components/Home/Band.vue
+++ src/components/Home/Band.vue
@@ -3,7 +3,7 @@
         <div class="bg-emerald-700 p-4 flex items-center justify-center flex-col text-white">
         
                 <!----<h1 class="text-2xl ">Foundation's Rallying Call</h1>-->
-            <h2 class="text-xl"><em>"Educate a child, Transform a nation."</em></h2>
+            <h2 class="text-xl"><em>"Educate a Child, Transform a Nation."</em></h2>
             
         </div>
     </div>
