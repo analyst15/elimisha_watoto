610849de23e8347f664155a3decc4d2435d8bca2
Alex Okeyo <alexokeyo15@gmail.com>
2025-02-11 13:15:46 +0300

    Capitalize the word child and nation and Italicize the slogan all together

--- src/components/Home/Programs.vue
+++ src/components/Home/Programs.vue
@@ -1,28 +1,25 @@
 <template>
-                <div class="text-center py-8 md:py-2">
-                    <h1 class="text-4xl mb-4 font-extrabold">  <span class="underline underline-offset-8 decoration-emerald-700 decoration-4">Our Pro</span>grams</h1>
-
-                </div>
-    <div class="flex flex-wrap items-center  justify-center my-8">
-        <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
-            <i class="material-icons text-emerald-700 text-8xl">laptop_mac</i>
-            <h1 class="mb-4 font-bold">Student Support</h1>
-            <p class="mx-8">At the heart of Elimisha Watoto Foundation is the Office of Student Liaison. The Student Liaison Officer is the link between the Foundation, our beneficiaries and their parents, and the school administration</p>
-        </div>
-        <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
-            <i class="material-icons text-emerald-700 text-8xl">local_library</i>
-            <h1 class="mb-4 font-bold">Academic Mentorship</h1>
-            <p class="mx-8">We are dedicated to enhancing academic performance of our beneficiaries by providing personalized mentorship and actively monitoring their welfare.</p>
-        </div>
-        <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
-            <i class="material-icons text-emerald-700 text-8xl">auto_stories</i>
-            <h1 class="mb-4 font-bold">Leap</h1>
-            <p class="mx-8">The Life Experience and Preparation Programme is a key component of our capacity-building efforts, aimed at empowering our beneficiaries with skills and knowledge to navigate their life journey successfully.</p>
-        </div>
-    </div>
+        <div class=" hidden md:block">
+            <InvolvedBox/>
+            </div>
 </template>
 
-<script setup>
+<script>
+import InvolvedBox from "../../components/InvolvedBox.vue";
+
+export default {
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+    }
+  },
+  components: {
+      InvolvedBox
+
+    },
+};
 
 </script>
 
