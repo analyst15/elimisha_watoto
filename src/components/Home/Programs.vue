e25f3d8621b6e9bb6d822798d54d4853049e57c9
Alex Okeyo <alexokeyo15@gmail.com>
2025-02-14 16:31:47 +0300

    Homepage changes

--- src/components/Home/Programs.vue
+++ src/components/Home/Programs.vue
@@ -1,4 +1,51 @@
 <template>
+  <div class="text-center py-8 md:py-2">
+    <h1 class="text-4xl mb-4 font-extrabold"> <span
+        class="underline underline-offset-8 decoration-emerald-700 decoration-4">Our Pro</span>grams</h1>
+
+  </div>
+  <div class="flex flex-wrap items-center  justify-center my-8">
+    <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
+      <i class="material-icons text-emerald-700 text-8xl">local_library</i>
+      <h1 class="mb-4 font-bold">Mentor</h1>
+      <p class="mx-8">We are dedicated to improving
+        our beneficiaries' termly
+        academic performance through
+        personalized mentorship.</p>
+      <div class="w-10 h-10 bg-emerald-700 text-white rounded-full flex items-center justify-center font-bold text-lg">
+        1
+      </div>
+    </div>
+    <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
+      <i class="material-icons text-emerald-700 text-8xl">laptop_mac</i>
+      <h1 class="mb-4 font-bold">Support</h1>
+      <p class="mx-8">Our liaison officers are expertsin their fields, offeringdedicated psychosocialsupport to our
+        beneficiaries.</p>
+      <div class="w-10 h-10 bg-emerald-700 text-white rounded-full flex items-center justify-center font-bold text-lg">
+        2
+      </div>
+    </div>
+    <div class="w-80 h-96 flex justify-center flex-col items-center border border-emerald-700 rounded-lg m-2">
+      <i class="material-icons text-emerald-700 text-8xl">auto_stories</i>
+      <h1 class="mb-4 font-bold">Empower</h1>
+      <p class="mx-8">Through capacity-buildingefforts, our program is designedto empower our beneficiairieswith
+        essential life skills.</p>
+      <div class="w-10 h-10 bg-emerald-700 text-white rounded-full flex items-center justify-center font-bold text-lg">
+        3
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
+
+
+<!----
+<template>
         <div class=" hidden md:block">
             <InvolvedBox/>
             </div>
@@ -26,3 +73,5 @@ export default {
 <style lang="scss" scoped>
 
 </style>
+
+-->
