41afb66366b561b11a3491a3477ee57c9b9049bd
Alex Okeyo <alexokeyo15@gmail.com>
2025-03-26 15:09:36 +0300

    adding cta button

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -22,20 +22,26 @@
         <h3 class="font-light text-3xl"></h3>
       </div>
     </section>-->
-
     <div
-      class="relative bg-[length:1100px_950px] lg:bg-[length:1500px_1000px] bg-left h-2/3 pt-80 bg-fixed"
+      class="relative bg-[length:1100px_950px] lg:bg-[length:1500px_1000px] bg-left h-2/3 pt-64 bg-fixed"
       style="
-        background-image: url('https://images.unsplash.com/photo-1536337005238-94b997371b40?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+        background-image: url('https://images.unsplash.com/photo-1536337005238-94b997371b40?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fHx8fA%3D%3D');
         background-position: center;
-      ">
-      <div class="">
-        <div class="pl-4 md:pl-20 text-white pb-40">
-          <!-- Place your text here -->
-          <span class="font-bold text-4xl lg:text-8xl"
-            >Elimisha Watoto Foundation</span
+      "
+    >
+    <div class="absolute inset-0 bg-black opacity-30"></div>
+      <div class="relative pl-4 md:pl-20 text-white pb-40">
+        <span class="font-bold text-4xl lg:text-8xl">Elimisha Watoto Foundation</span>
+        <p class="text-2xl italic">Educate a Child, Transform a Nation</p>
+
+        <!-- CTA Buttons -->
+        <div class="mt-6 flex">
+          <a
+            href="/contact"
+            class="bg-orange-500 hover:bg-emerald-700 text-white py-3 px-20 rounded-lg text-2xl transition duration-300"
           >
-          <p class="text-2xl italic">Educate a Child, Transform a Nation</p>
+            DONATE
+          </a>
         </div>
       </div>
     </div>
@@ -57,13 +63,6 @@ import Slider from "../components/Home/Slider.vue";
 import Kcse from "../components/Home/Kcse.vue";
 
 export default {
-  setup() {
-    return {};
-  },
-  computed: {},
-  methods: {
-    example() {},
-  },
   components: {
     Card,
     Band,
