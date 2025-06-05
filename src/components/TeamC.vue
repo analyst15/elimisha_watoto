8cb2590b4030ff964ebdde1a1f53b0f42f6c6e00
Alex Okeyo <alexokeyo15@gmail.com>
2025-06-05 12:50:53 +0300

    update to team members

--- src/components/TeamC.vue
+++ src/components/TeamC.vue
@@ -39,12 +39,12 @@
                 <span class="text-xl pt-2">Assistant Liaison Officer</span>
             </div>
             <div class="flex flex-col mt-8 items-center">
-                <img src="../assets/Ashley.png" alt="" class="rounded-full w-64 h-64">
+                <img src="../assets/AshleyOtieno.jpg" alt="" class="rounded-full w-64 h-64">
                 <span class="font-bold text-xl pt-2 ">Ashley Otieno</span>
                 <span class="text-xl pt-2">Liaison Officer</span>
             </div>
             <div class="flex flex-col mt-8 items-center">
-                <img src="../assets/Warren.png" alt="" class="rounded-full w-64 h-64">
+                <img src="../assets/WarrenBahati.jpg" alt="" class="rounded-full w-64 h-64">
                 <span class="font-bold text-xl pt-2 ">Warren Bahati</span>
                 <span class="text-xl pt-2">Liaison Officer</span>
             </div>
